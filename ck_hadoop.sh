d1620476b45ccb1f6272128738d0b0406cd11e0c
neojarvis <techadmin@iamneo.ai>
2025-06-18 17:20:27 +0000

    Auto-save-commit-18/06/2025--22:50:23

--- ck_hadoop.sh
+++ ck_hadoop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Starting Hadoop daemons..."
+# echo "Starting Hadoop daemons..."
 # hdfs --daemon start namenode
 # hdfs --daemon start datanode
 # hdfs --daemon start secondarynamenode
 # yarn --daemon start resourcemanager
 # yarn --daemon start nodemanager
-echo "Hadoop started!"
+# echo "Hadoop started!"
  
 echo "🔍 Checking Hadoop..."
  
